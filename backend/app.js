9e55a173228616923e59699659edba48e0a7c022
KingJr7 <imboujunior91@gmail.com>
2025-04-05 04:13:32 +0200

    ajout de la base du système de connexion

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const http = require('http')
 const cors = require('cors')
 const WebSocket = require('ws')
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express()
 const server = http.createServer(app)
 
 const wss = new WebSocket.Server({ server })
 
+const SECRET_KEY = "kingjr7";
 
 wss.on("connection", (ws) => {
   console.log("Nouveau client connécter");
@@ -22,10 +26,24 @@ wss.on("connection", (ws) => {
   })
 })
 
+const corsOptions = {
+  origin: "http://localhost:3001", // Frontend : autoriser cette origine
+  methods: ["GET", "POST", "PUT", "DELETE"], // Méthodes autorisées
+  credentials: true, // Permet l'envoi des cookies
+};
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
+// app.use(bodyParser.json());
 
+// Ajoute ceci pour autoriser les cookies en CORS si tu as un frontend séparé
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // frontend
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 const createClient = require("@supabase/supabase-js")
 
@@ -33,11 +51,20 @@ const supabaseUrl = 'https://pxldlplqpshfigfejuam.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bGRscGxxcHNoZmlnZmVqdWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0NTU0MTgsImV4cCI6MjA1ODAzMTQxOH0.9_xwVw5dUk3eIEte2uQzuqaAyAi-YXqPKpFNRFXv-3c'
 const supabase = createClient.createClient(supabaseUrl, supabaseKey)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Middleware d'authentification via le cookie
+function authenticateToken(req, res, next) {
+  const token = req.cookies.token;
 
-app.get('/get-messages', async (req, res) => {
+  if (!token) return res.status(401).json({ message: 'Pas de token' });
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token invalide ou expiré' });
+    req.user = user;
+    next();
+  });
+}
+
+app.get('/get-messages' ,async (req, res) => {
   const { data, error } = await supabase.from('messages').select()
   
   if (error) {
@@ -49,7 +76,42 @@ app.get('/get-messages', async (req, res) => {
 }
 )
 
-app.post('/send-message', async (req, res) => {
+app.post('/login', async (req, res) => {
+  const {pseudo, email} = req.body
+
+  
+  const {data, error}  = await supabase.from('users_chat').select().eq('pseudo', pseudo).eq('email', email)
+
+  if (error) {
+    console.error('Erreur lors de la récupération des messages :', error);
+    return res.status(500).json({ error: 'Erreur lors de la récupération des messages' });
+  }
+  
+  if (data.length === 0) {
+    return res.status(401).json({ message: 'Utilisateur non trouvé' });
+  }else {
+    const token = jwt.sign(
+      { id: data.id, pseudo: data.pseudo, email: data.email, role: data.role }, 
+      SECRET_KEY, 
+      { expiresIn: '1h' }
+    );
+    
+    // ⚠️ Ici on envoie le cookie
+    res.cookie('token', token, {
+      httpOnly: true,          // protégé contre XSS
+      secure: false,           // à true en production (HTTPS obligatoire)
+      sameSite: 'lax',         // ou 'strict' ou 'none' selon ton besoin
+      maxAge: 3600000          // 1h
+    });
+  
+    console.log(data);
+    return res.status(200).json(data)
+  }
+  
+})
+
+
+app.post('/send-message', authenticateToken ,async (req, res) => {
   const { message, pseudo} = req.body;
 
   const {Inserterror} = await supabase.from('messages').insert({
@@ -80,6 +142,6 @@ app.post('/send-message', async (req, res) => {
 
 const port = 3000
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0',() => {
   console.log(`serveur en écoute sur le port ${port}`)
 })
--- bluevision/app/page.tsx
+++ bluevision/app/page.tsx
@@ -2,9 +2,15 @@
 import styles from "./page.module.css";
 import Message from "./components/message";
 import { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCommentDots, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { faDesktop, faTabletAlt } from '@fortawesome/free-solid-svg-icons';
+import { faAndroid } from '@fortawesome/free-brands-svg-icons';
+import { faApple } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
-  const myPseudo = "kingjr7";
+  const myPseudo = localStorage.getItem("pseudo");
+  const myEmail = localStorage.getItem("email");
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
 
@@ -18,10 +24,14 @@ export default function Home() {
       console.log(data);
       setMessages([...data]);
     };
+
   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:3000/get-messages")
+    fetch("http://localhost:3000/get-messages", {
+      method: "GET",
+      credentials: 'include',
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -44,6 +54,7 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: 'include',
       body: JSON.stringify({
         message: message,
         pseudo: myPseudo,
@@ -61,52 +72,40 @@ export default function Home() {
 
   return (
     <div className={styles.page}>
-      <header className={styles.header}>
-        <h1>Bienvenue sur BLUEVISION</h1>
-        <p>Discutez anonymement avec d'autres utilisateurs en temps réel.</p>
+      <div className={styles.app_container}>
+      <header className={styles.app_header}>
+        <div className={styles.header_content}>
+          <h1><FontAwesomeIcon icon={faCommentDots}/> BLUEVISION</h1>
+          <div className={styles.user_info} id="user-info">{myPseudo}({myEmail})</div>
+        </div>
       </header>
 
-      <main className={styles.main}>
-        <section className={styles.chat}>
-          <div className={styles.messages}>
-            {messages.map((message) => (
-              <Message
-                key={message.id}
-                text={message.content}
-                sender={message.pseudo}
-                isMy={message.pseudo === myPseudo}
-              />
-            ))}
-          </div>
-          <div className={styles.message_input}>
-            <input
-              type="text"
-              className={styles.message}
-              placeholder="Écrivez votre message..."
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  sendMessage();
-                  setMessage("");
-                }
-              }}
-            />
-            <button className={styles.send} onClick={sendMessage}>
-              Envoyer
-            </button>
-          </div>
-        </section>
+      <main className={styles.chat_container}>
+        <div className={styles.chat_messages} id="messages">
+          {messages.map((message, index) => (
+            <Message key={index} text={message.content} sender={message.pseudo} isMy={message.pseudo === myPseudo}/>
+          ))}
+        </div>
+        
+        <div className={styles.message_input}>
+          <input type="text" id="message" placeholder="Écrivez votre message..." autoComplete="off" value={message} onChange={(e) => {setMessage(e.target.value)}}/>
+          <button id="send" onClick={sendMessage}><FontAwesomeIcon icon={faPaperPlane}/></button>
+        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <p>&copy; 2025 BLUEVISION. Tous droits réservés.</p>
-        <p>Créateurs : Emmanuel Sasaki & Emmanuel Kingjr7</p>
-        <p>
-          Contact :{" "}
-          <a href="mailto:sasaki-compagnie@gmail.com">team Emmanuel</a>
-        </p>
+      <footer className={styles.app_footer}>
+        <div className={styles.footer_content}>
+          <p>© 2025 BLUEVISION - Tous appareils</p>
+          <p>Creation de la L'équipe Sasaki</p>
+          <div className={styles.device_badges}>
+          <FontAwesomeIcon icon={faAndroid} />
+          <FontAwesomeIcon icon={faApple} />
+          <FontAwesomeIcon icon={faDesktop} />
+          <FontAwesomeIcon icon={faTabletAlt} />
+          </div>
+        </div>
       </footer>
+      </div>
     </div>
   );
 }
